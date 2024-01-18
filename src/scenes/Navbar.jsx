33b06d5ad75df4459b9893a41d2e2a70c00c16f0
Breck Houghton <breck.houghton@gmail.com>
2024-01-18 12:13:42 -0800

    Building out Navbar

---
+++ src/scenes/Navbar.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow" : ""
+      } hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+// eslint-disable-next-line
+const Navbar = ({ selectedPage, setSelectedPage }) => {
+  // eslint-disable-next-line
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  // eslint-disable-next-line
+  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <nav className={`z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold">BRH</h4>
+
+        {/* DESKTOP NAV */}
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
